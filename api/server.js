2a03cf36847fb8b614f6d9e68b61acd9877a8cb4
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-29 14:02:06 -0500

    GET user by ID completed

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ server.post('/api/users', async (req, res) => {
       res.status(201).json(newUser);
     }
   } catch (err) {
-    res.status(500).send({
+    res.status(500).json({
       message: `Error: ${err.message}`,
     });
   }
@@ -40,7 +40,25 @@ server.get('/api/users', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
   } catch (err) {
-    res.status(500).send({
+    res.status(500).json({
+      message: `Error: ${err.message}`,
+    });
+  }
+});
+
+// [GET] /api/users/:id
+server.get('/api/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({
+        message: `User with id ${req.params.id} does not exist`,
+      });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({
       message: `Error: ${err.message}`,
     });
   }
