20334be85940d549d95f21cd7114ba89014f8c4c
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-29 14:16:14 -0500

    PUT completed

--- api/server.js
+++ api/server.js
@@ -8,14 +8,6 @@ const server = express();
 // global middleware
 server.use(express.json());
 
-// | Method | URL            | Description                                                                                            |
-// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
-// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
-// | GET    | /api/users     | Returns an array users.                                                                                |
-// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
-// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
-// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
-
 // [POST] /api/users
 server.post('/api/users', async (req, res) => {
   try {
@@ -75,6 +67,33 @@ server.delete('/api/users/:id', async (req, res) => {
     } else {
       const deletedUser = await User.remove(req.params.id);
       res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `Error: ${err.message}`,
+    });
+  }
+});
+
+// [PUT] /api/users/:id
+server.put('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    if (!body.name || !body.bio) {
+      res.status(400).json({
+        message: 'Missing name or bio',
+      });
+    } else {
+      const user = await User.findById(id);
+      if (!user) {
+        res.status(404).json({
+          message: `User with id ${req.params.id} does not exist`,
+        });
+      } else {
+        const updatedUser = await User.update(id, body);
+        res.status(200).json(updatedUser);
+      }
     }
   } catch (err) {
     res.status(500).json({
